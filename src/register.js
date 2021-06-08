07a469fab6ffb1a59a109a0b06f7427abb0da5f8
Jeyanth Kishore Ramasamy <jeyanth.kishore@gmail.com>
2021-06-08 15:39:32 -0300

    changes

--- src/register.js
+++ src/register.js
@@ -75,7 +75,7 @@ class Register extends Component {
     };
     console.log("Check")
     await axios
-      .post("http://localhost:8080/register", register)
+      .post("https://registerpage-oiny77almq-uc.a.run.app/register", register)
       .then((response) => {
         Swal.fire("Registration Successfull");
         this.props.history.push("/login");
